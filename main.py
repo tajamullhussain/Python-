3e0b1dcf136dede70c0dde9aabc7dc6e3c315a0d
tajamullhussain <tajamullh6@gmail.com>
2026-04-08 03:17:08 +0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Day 1
+
+# introduction to python?
+# python is a high level programming language which is used for web development , data science , machine learning , AI , etc. easy to use and learn and look i like english language. beginner friendly language. 
+
+# print()  : print function is used to print the output on the screen.
+
+print("hello world!") #most common used in python
+
+print(1) #integer value can also be printed
+
+print('hello world! 1') # single quote is also used in python
+
+print('''hello world! 2''')  # 3 single quote is also used in python.
